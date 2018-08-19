583d569009705261a62eabf2d1b8a0de3e6e6a0b
Nepxion <nepxion@aliyun.com>
2018-08-19 12:31:48 +0800

    升级版本到1.0.7

--- src/main/java/com/nepxion/zxing/constant/ZxingConstant.java
+++ src/main/java/com/nepxion/zxing/constant/ZxingConstant.java
@@ -31,7 +31,7 @@ public class ZxingConstant {
         System.out.println("╚════╩╝╚╩╩╝╚╩═╗║");
         System.out.println("            ╔═╝║");
         System.out.println("            ╚══╝");
-        System.out.println("Nepxion Zxing  v1.0.6");
+        System.out.println("Nepxion Zxing  v1.0.7");
         System.out.println("");
     }
 }
